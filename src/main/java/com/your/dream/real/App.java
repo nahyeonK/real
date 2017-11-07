8f53a76ab6afacb233fe9eb77260d78a579ad080
nhKwon <knhsponge@naver.com>
2017-11-07 16:26:03 +0900

    exercise

--- src/main/java/com/your/dream/real/App.java
+++ src/main/java/com/your/dream/real/App.java
@@ -15,8 +15,8 @@ public class App
 	{
 		// do not create App class directly
 	}
-	
-    public static void main( String[] args ) {
+
+	public static void main(String[] args) {
     	for(String arg : args) {
     		logger.log(Level.FINE, "parameter: {}", arg);
     	}
